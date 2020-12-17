a8a5a5e88067e6db6b90af79341bafe7c371872b
Square789 <46634729+Square789@users.noreply.github.com>
2020-12-17 16:20:12 +0100

    Updated solution for y2020 d17

--- y2020/d17.py
+++ y2020/d17.py
@@ -1,10 +1,11 @@
 from aoc_input import get_input
 import aoc_helpers as ah
 
+from itertools import product
 import numpy as np
 
 # Good chance this will fail for other inputs since the array bounds
-# are somewhat botched.
+# are somewhat botched and will wrap around in a really unclean manner.
 
 DAY = 17
 YEAR = 2020
@@ -15,40 +16,23 @@ class STATE:
 	INACTIVE = 0
 	ACTIVE = 1
 
-def step3(array):
+def step(array):
 	new_array = array.copy()
-	for z in range(array.shape[0]):
-		for y in range(array.shape[1]):
-			for x in range(array.shape[2]):
-				actneightbors = (array[z-1:z+2, y-1:y+2, x-1:x+2] == STATE.ACTIVE).sum()
-				if array[z, y, x] == STATE.ACTIVE:
-					# -1 since cube itself is counted
-					if not (2 <= actneightbors - 1 <= 3):
-						new_array[z, y, x] = STATE.INACTIVE
-				else:
-					if actneightbors == 3:
-						new_array[z, y, x] = STATE.ACTIVE
-	return new_array
-
-def step4(array):
-	new_array = array.copy()
-	for w in range(array.shape[0]):
-		for z in range(array.shape[1]):
-			for y in range(array.shape[2]):
-				for x in range(array.shape[3]):
-					actneightbors = (array[w-1:w+2, z-1:z+2, y-1:y+2, x-1:x+2] == STATE.ACTIVE).sum()
-					if array[w, z, y, x] == STATE.ACTIVE:
-						# -1 since cube itself is counted
-						if not (2 <= actneightbors - 1 <= 3):
-							new_array[w, z, y, x] = STATE.INACTIVE
-					else:
-						if actneightbors == 3:
-							new_array[w, z, y, x] = STATE.ACTIVE
+	for pos in product(*map(range, array.shape)):
+		actneightbors = (
+			array[tuple(slice(coord - 1, coord + 2) for coord in pos)] == STATE.ACTIVE
+		).sum()
+		if array[pos] == STATE.ACTIVE:
+			if not (2 <= actneightbors - 1 <= 3):
+				new_array[pos] = STATE.INACTIVE
+		else:
+			if actneightbors == 3:
+				new_array[pos] = STATE.ACTIVE
 	return new_array
 
 def sol0(pzin):
 	arr = np.zeros(
-		(3+2*STEPS, len(pzin)+2*STEPS, len(pzin[0])+2*STEPS),
+		(3 + 2*STEPS, len(pzin) + 2*STEPS, len(pzin[0]) + 2*STEPS),
 		dtype=np.uint8
 	)
 	for y, ln in enumerate(pzin):
@@ -56,20 +40,21 @@ def sol0(pzin):
 			arr[1+STEPS, y + STEPS, x + STEPS] = STATE.ACTIVE if c == "#" else STATE.INACTIVE
 
 	for _ in range(STEPS):
-		arr = step3(arr)
+		arr = step(arr)
 	return (arr == STATE.ACTIVE).sum()
 
 def sol1(pzin):
 	arr = np.zeros(
-		(3+2*STEPS, 3+2*STEPS, len(pzin)+2*STEPS, len(pzin[0])+2*STEPS),
+		(3 + 2*STEPS, 3 + 2*STEPS, len(pzin) + 2*STEPS, len(pzin[0]) + 2*STEPS),
 		dtype=np.uint8
 	)
 	for y, ln in enumerate(pzin):
 		for x, c in enumerate(ln):
-			arr[1+STEPS, 1+STEPS, y + STEPS, x + STEPS] = STATE.ACTIVE if c == "#" else STATE.INACTIVE
+			arr[1 + STEPS, 1 + STEPS, y + STEPS, x + STEPS] = \
+				STATE.ACTIVE if c == "#" else STATE.INACTIVE
 
 	for _ in range(STEPS):
-		arr = step4(arr)
+		arr = step(arr)
 	return (arr == STATE.ACTIVE).sum()
 
 def main():
